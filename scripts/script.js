bdb7435261db3f292447abd86778965e7b367202
Prasad <prasadshinde1120@gmail.com>
2023-02-15 00:45:42 -0600

    First commit

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+
+
+function handleHamburger() {
+  var x = document.getElementById("demo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function copyToClipboard(str) {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+
+  alert("Email-ID has been copied to the clipboard ✅");
+};
